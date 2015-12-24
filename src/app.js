80df5bc9a46c8369859eb87bd19395093cbf27b2
Daniel de la Cruz <daniel.delacruz@scmspain.com>
2015-12-24 09:54:24 +0100

    added Calendar class, able to draw any month's calendar

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
-let element = document.createElement('h3');
-element.innerHTML = 'Hello schedulr!';
-document.body.appendChild(element);
+import Calendar from './domain/calendar';
+
+const calendar = new Calendar();
+
+let table = document.createElement('table');
+calendar.monthDays(2015, 11).forEach((week) => {
+  let row = table.appendChild(document.createElement('tr'));
+  week.forEach((day) => {
+    let cell = row.appendChild(document.createElement('td'));
+    cell.innerHTML = day.toString();
+  });
+});
+
+document.body.appendChild(table);
---
+++ src/domain/calendar.js
@@ -0,0 +1,70 @@
+export default class Calendar {
+  constructor(firstWeekDay = 0){
+    this.firstWeekDay = firstWeekDay;
+  }
+
+  _weekStartDate(date) {
+    var startDate = new Date(date.getTime());
+    while (startDate.getDay() !== this.firstWeekDay) {
+      startDate.setDate(startDate.getDate() - 1);
+    }
+    return startDate;
+  }
+
+  _monthDates(year, month, dayFormatter, weekFormatter) {
+    if ((typeof year !== "number") || (year < 1970)) {
+      throw new Error('year must be a number >= 1970');
+    };
+    if ((typeof month !== "number") || (month < 0) || (month > 11)) {
+      throw new Error('month must be a number (Jan is 0)');
+    };
+
+    var weeks = [],
+    week = [],
+    i = 0,
+    date = this._weekStartDate(new Date(year, month, 1));
+    do {
+      for (i=0; i<7; i++) {
+        week.push(dayFormatter ? dayFormatter(date) : date);
+        date = new Date(date.getTime());
+        date.setDate(date.getDate() + 1);
+      }
+      weeks.push(weekFormatter ? weekFormatter(week) : week);
+      week = [];
+    } while ((date.getMonth()<=month) && (date.getFullYear()===year));
+
+    return weeks;
+  }
+
+  monthDays(year, month) {
+    var getDayOrZero = function getDayOrZero(date) {
+      return date.getMonth() === month ? date.getDate() : 0;
+    };
+    return this._monthDates(year, month, getDayOrZero);
+  }
+};
+
+Calendar.monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+Calendar.dayNames = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday'
+];
